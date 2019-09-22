dee3404d9e1dd33801b5ab1e226882c6ec9e7d3f
juanm <juanm@DESKTOP-JEDE2QC>
2019-09-22 18:53:44 +0200

    "Final autoevaluacion"

--- NominasRepaso/src/main/java/Laboral/CalculadoraNominas.java
+++ NominasRepaso/src/main/java/Laboral/CalculadoraNominas.java
@@ -15,77 +15,161 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 import baseDatos.BBDD;
+import baseDatos.Backup;
 
 public class CalculadoraNominas {
 
 	public static void main(String[] args) throws DatosNoCorrectosException, IOException, ClassNotFoundException, SQLException {
 		// TODO Auto-generated method stub
 		
-		//Creacion de fichero empleados
-		File empleados = new File("empleados.txt");
-		
-		FileReader fr = new FileReader (empleados);
-		BufferedReader br = new BufferedReader(fr);
-		String lectura1 = br.readLine();
-		String[] registro1 = lectura1.split(",");
-		
-//		
-//		
-		Empleado emp1 = new Empleado(registro1[0], registro1[1], registro1[2].charAt(0), Integer.parseInt(registro1[3]), Integer.parseInt(registro1[4]));
-//		
-//		
-//		String lectura2 = br.readLine();
-//		String[] registro2 = lectura2.split(",");
-//		
-//		br.close();
-//		Empleado emp2 = new Empleado(registro2[0], registro2[1], registro2[2].charAt(0));
-//		
-//		System.out.println(escribe(emp1, emp2));
-//		
-//		emp1.setCategoria(9);
-//		registro1[3] = Integer.toString(emp1.getCategoria());
-//		emp2.incrAnyo();
-//		registro2[3] = Integer.toString(emp2.anyos);
-//		
-//		empleados.delete();
-//		FileWriter fw = new FileWriter(empleados);
-//		BufferedWriter bw = new BufferedWriter(fw);
-//		
-//		for (int i = 0; i < registro1.length; i++) {
-//			bw.write(registro1[i] + ",");
-//		}
-//		bw.write("\n");
-//		
-//		for (int i = 0; i < registro2.length; i++) {
-//			bw.write(registro2[i] + ",");
-//		}
-//		bw.close();
-//		
-//		
-//		 FileOutputStream fos = new FileOutputStream("datos.dat");
-//		 DataOutputStream dos = new DataOutputStream(fos);
-//		
-//		fos.write(registro1[1].getBytes());
-//		fos.write(Nomina.sueldo(emp1));
-//		fos.write(registro2[2].getBytes());
-//		fos.write(Nomina.sueldo(emp2));
-//		
-//		
-//		
-		
+			
+//		File altaEmpleados = new File("C:\\Users\\juanm\\git\\DWES\\NominasRepaso\\altaNuevosEmpleados.txt");
 		BBDD bbdd = new BBDD();
-		System.out.println(bbdd.mostrarEmpleados().toString());
-		System.out.println();
-		bbdd.altaEmpleado(new Empleado("fas", "fdsafds", 's', 6, 5));
-		System.out.println(bbdd.buscarSalario("32000032G"));
+		Backup backup = new Backup(bbdd);
+		boolean salir = false;		
 		
+		Scanner numeros = new Scanner(System.in);
+		Scanner palabras = new Scanner(System.in);
+				
+		while (!salir) {
+			
+			System.out.println("1. Ver todos los empleados \n"
+					+ "2. Ver salario de un empleado especifico \n"
+					+ "3. Submenu de edicion \n"
+					+ "4. Recalcular y actualizar sueldo de un empleado \n"
+					+ "5. Recalcular y actualizar sueldos de todos los empleados \n"
+					+ "6. Realizar copia de seguridad de la BBDD en fichero \n"
+					+ "Pulsa cualquier otra tecla para salir");
+			String opcionMenu = palabras.nextLine();
+			
 		
-		
-	}
+			switch (opcionMenu) {
+				case "1": 
+					
+					for (String empleado : bbdd.mostrarEmpleados()) {
+						System.out.println(empleado);
+					}
+					
+					break;
+				case "2":
+					
+					System.out.println("Introduce el dni: ");
+					String dni = palabras.nextLine();
+					System.out.println(bbdd.buscarSalario(dni));
+					
+					
+					break;
+				case "3":
+					System.out.println("Introduce DNI para editar el empleado");
+					String dniEditar = palabras.nextLine();
+					String nombreNuevo;
+					String sexoNuevo;
+					int categoriaNueva;
+					int anyosNuevos;
+					
+					
+						System.out.println("Elige un campo para editar \n"
+								+ "1. Nombre \n"
+								+ "2. Sexo \n"
+								+ "3. Categoria \n"
+								+ "4. Anyos \n"
+								+ "5. Todos los campos \n"
+								+ "Introduce cualquier otra tecla para salir.");
+						
+						
+							Empleado empleadoParaEditar = bbdd.buscarEmpleado(dniEditar);
+							String opcionSubmenu = numeros.nextLine();
+							
+						switch (opcionSubmenu) {
+						case "1":
+							System.out.println("Elija el nuevo nombre");
+							nombreNuevo = palabras.nextLine();
+							bbdd.editarEmpleado(new Empleado(nombreNuevo, dniEditar, empleadoParaEditar.sexo, empleadoParaEditar.getCategoria(), empleadoParaEditar.anyos));
+							
+							break;
+						case "2":
+							System.out.println("Elija el nuevo sexo");
+							sexoNuevo = palabras.nextLine();
+							bbdd.editarEmpleado(new Empleado(empleadoParaEditar.nombre, dniEditar, sexoNuevo.charAt(0), empleadoParaEditar.getCategoria(), empleadoParaEditar.anyos));
 
-	private static String escribe(Empleado emp1, Empleado emp2) {
-		return emp1.toString() + " Gana: " + Nomina.sueldo(emp1) + emp2.toString() + " Gana: " + Nomina.sueldo(emp2);
-	}
+							
+							
+							break;
+						case "3":
+							System.out.println("Elija la nueva categoria");
+							categoriaNueva = numeros.nextInt();
+							bbdd.editarEmpleado(new Empleado(empleadoParaEditar.nombre, dniEditar, empleadoParaEditar.sexo, categoriaNueva, empleadoParaEditar.anyos));
+
+							break;
+						case "4":
+							System.out.println("Elija los nuevos anyos");
+							anyosNuevos = numeros.nextInt();
+							bbdd.editarEmpleado(new Empleado(empleadoParaEditar.nombre, dniEditar, empleadoParaEditar.sexo, empleadoParaEditar.getCategoria(), anyosNuevos));
+
+							break;
+						case "5":
+							System.out.println("Elija el nuevo nombre");
+							nombreNuevo = palabras.nextLine();
+							
+							System.out.println("Elija el nuevo sexo");
+							sexoNuevo = palabras.nextLine();
+							
+							System.out.println("Elija la nueva categoria");
+							categoriaNueva = numeros.nextInt();
+							
+							System.out.println("Elija los nuevos anyos");
+							anyosNuevos = numeros.nextInt();
+							
+							bbdd.editarEmpleado(new Empleado(nombreNuevo, dniEditar, sexoNuevo.charAt(0), categoriaNueva, anyosNuevos));
+
+							break;
+						default:
+							System.out.println("");
+							break;
+						}
+						
+					
+					break;
+					
+				case "4":
+					System.out.println("Introduce el dni para recalcular el sueldo");
+					dniEditar = palabras.nextLine();
+					bbdd.calcularSueldo(bbdd.buscarEmpleado(dniEditar));
+					
+					
+					break;
+					
+				case "5":
+					for(String registro : bbdd.mostrarEmpleados()) {
+						dni = registro.split(",")[1];
+						bbdd.calcularSueldo(bbdd.buscarEmpleado(dni));
+					}
+					
+					break;
+					
+				case "6":
+					
+					backup.backupbbddafichero();
+					
+					break;
+					
+				
+					
+				default:
+					salir=true;
+					break;
+			}
+		
 	
+		};
+			
+
+	}
 }
+		
+	
+
+	
+	
+
 
--- NominasRepaso/src/main/java/baseDatos/BBDD.java
+++ NominasRepaso/src/main/java/baseDatos/BBDD.java
@@ -1,5 +1,7 @@
 package baseDatos;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -8,9 +10,11 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import com.mysql.cj.xdevapi.Result;
 
+import Laboral.DatosNoCorrectosException;
 import Laboral.Empleado;
 import Laboral.Nomina;
 
@@ -32,11 +36,25 @@ public class BBDD {
 		String salario = "insert into Nominas values (" + Nomina.sueldo(emp) + ',' + emp.dni + ")";
 		stmt.execute(salario);
 		
-//		if(emplexistente(emp.dni) == null) {
-//			stmt.executeUpdate(consulta);
-//		}else {
-//			throw new DatosNoCorrectosException("Empleado con ese DNI ya existente");
-//		}
+	}
+	
+	public void altaEmpleados(File fichero) throws FileNotFoundException, NumberFormatException, DatosNoCorrectosException, SQLException {
+		List<Empleado> empleados = new ArrayList<Empleado>();
+		Scanner sc = new Scanner(fichero);
+		String[] registro;
+		while(sc.hasNext()) {
+			registro = sc.nextLine().split(",");
+			if(registro.length > 3) {
+				empleados.add(new Empleado(registro[0], registro[1], registro[2].charAt(0), Integer.parseInt(registro[3]), Integer.parseInt(registro[4])));
+			}else {
+				empleados.add(new Empleado(registro[0], registro[1], registro[2].charAt(0)));
+			}
+		}
+		
+		for (Empleado emp : empleados) {
+			altaEmpleado(emp);
+		}
+		
 	}
 
 	public List<String> mostrarEmpleados() throws ClassNotFoundException, SQLException {
@@ -69,5 +87,49 @@ public class BBDD {
 
 		return salario;
 	}
+	
 
+	public Empleado buscarEmpleado(String dni) throws SQLException, DatosNoCorrectosException {
+		Empleado empleado = null;
+		String query = "select * from Empleados where dni like ?";
+		PreparedStatement ps = con.prepareStatement(query);
+		ps.setString(1, dni);
+		rs = ps.executeQuery();
+
+		
+		while(rs.next()) {
+			empleado = new Empleado(rs.getString("nombre"), 
+					rs.getString("dni"), 
+					rs.getString("sexo").charAt(0),
+					rs.getInt("categoria"),
+					rs.getInt("anyos"));
+		}
+		
+		return empleado;
+	}
+	
+	public void editarEmpleado(Empleado emp) throws SQLException {
+		String query = "update empleados set nombre = ?, sexo = ?, categoria = ?, anyos = ? where dni = ?";
+		PreparedStatement ps = con.prepareStatement(query);
+		ps.setString(1, emp.nombre);
+		ps.setString(2, Character.toString(emp.sexo));
+		ps.setInt(3, emp.getCategoria());
+		ps.setInt(4, emp.anyos);
+		ps.setString(5, emp.dni);
+
+		ps.executeUpdate();
+		calcularSueldo(emp);
+		
+	}
+
+	
+	public void calcularSueldo(Empleado emp) throws SQLException {
+		
+		String query = "update nominas set salario = "+Nomina.sueldo(emp)+" where dni = ?";
+		PreparedStatement ps = con.prepareStatement(query);
+		ps.setString(1, emp.dni);
+		ps.executeUpdate();
+	}
+	
+	
 }
